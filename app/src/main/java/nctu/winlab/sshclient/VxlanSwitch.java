eb42d593da268da17a1f85a40f758d4564fd687f
gamersloius <gamerslouis@gmail.com>
2021-09-23 01:25:14 +0800

    [feature] Add 5000 vxlan tenant system support

--- app/src/main/java/nctu/winlab/sshclient/VxlanSwitch.java
+++ app/src/main/java/nctu/winlab/sshclient/VxlanSwitch.java
@@ -7,5 +7,10 @@ public interface VxlanSwitch {
     public ObjectNode setVxlanVlan(String vnid, String vid);
     public ObjectNode setVxlanVtep(String vnid, String ip, String mac);
     public ObjectNode setVxlanStatus(boolean flag);
+    public ObjectNode setVxlanTenantSystemLocal(String vni, String mac, String port, boolean add);
+    public ObjectNode setVxlanTenantSystemRemote(String vni, String mac, String remoteIp, boolean add);
+    public ObjectNode showVxlanTenantSystemLocal();
+    public ObjectNode showVxlanTenantSystemRemove();
     public ObjectNode showVxlan();
+    public ObjectNode testConnection();
 }
